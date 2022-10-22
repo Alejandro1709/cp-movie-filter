a1d2d50c92d58b4423ceb7927778859182025005
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-10-22 18:16:18 -0500

    Add: Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <div className='Navbar'>
+        <nav role='navigation' className='Navbar__wrapper'>
+          <h1>Logo</h1>
+          <ul>
+            <li>Home</li>
+            <li>Movies</li>
+            <li>Theathers</li>
+            <li>Account</li>
+          </ul>
+        </nav>
+      </div>
       <p>Hello</p>
     </div>
   );
