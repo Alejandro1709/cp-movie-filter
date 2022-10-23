be5a722e11998769cb193579c3813e404faa04aa
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-10-22 19:11:48 -0500

    Add: FilterBar

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,29 @@ function App() {
           </ul>
         </nav>
       </div>
-      <p>Hello</p>
+      <main id='root' className='container'>
+        <div className='FilterBar'>
+          <ul className='FilterBar__list'>
+            <li className='FilterBar__list__item'>
+              <h3>Por pelicula</h3>
+              <span>Que quieres ver</span>
+            </li>
+            <li className='FilterBar__list__item'>
+              <h3>Por ciudad</h3>
+              <span>Donde estas?</span>
+            </li>
+            <li className='FilterBar__list__item'>
+              <h3>Por cine</h3>
+              <span>Elige tu Cineplanet</span>
+            </li>
+            <li className='FilterBar__list__item'>
+              <h3>Por fecha</h3>
+              <span>Elige un dia</span>
+            </li>
+          </ul>
+          <button>Apply</button>
+        </div>
+      </main>
     </div>
   );
 }
